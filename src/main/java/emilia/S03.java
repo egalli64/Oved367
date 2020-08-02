214f6ab87cd6432a08a89d49f67ab9c92243cde0
Emilia Faraci <emiliafaraci25@gmail.com>
2020-08-02 11:09:24 +0200

    Commit S03 ex

---
+++ src/main/java/emilia/S03.java
@@ -0,0 +1,156 @@
+package emilia;
+
+public class S03 {
+    /**
+     * Check if the parameter is positive, negative, or zero
+     * 
+     * @param value an integer
+     * @return "positive", "negative", or "zero"
+     */
+    public static String checkSign(int value) {
+       if(value>0) {
+        return "positive";
+       }else if (value <0) {
+        return "negative";
+       }
+       
+       return "zero";
+   
+    }
+    /**
+     * Check if the parameter is odd
+     * 
+     * @param value an integer
+     * @return "odd" or "even"
+     */
+    public static boolean isOdd(int value) {
+        
+    	if(value %2 !=0) {
+    		return true;
+    	}
+    	
+        return false;
+    }
+
+    /**
+     * Convert the parameter to its English name, if it is in [0..9]
+     * 
+     * @param value an integer
+     * @return "zero" for 0 ... "nine" for 9, or "other"
+     */
+    public static String asWord(int value) {
+       
+    	if (value >=0 && value<=9) {
+    	if (value == 0) {
+        	return "zero" ;
+        }else if (value ==1) {
+        	return "one";
+        }else if (value ==2) {
+        	return "two";
+        }else if (value ==3) {
+        	return "three";
+        }else if (value==4) {
+        	return "four";
+        }else if (value ==5) {
+        	return "five";
+        }else if (value ==6) {
+        	return "six";
+        }else if (value ==7) {
+        	return "seven";
+        }else if (value ==8) {
+        	return "eight";
+        }else if (value ==9) {
+        	return "nine";
+        }else if (value ==10) {
+        	return "ten";
+        }
+    }
+    	return "other";
+    }
+
+    /**
+     * Convert a value in the interval [0, 100] to a letter in [A, F]
+     * 
+     * A if value > 90 B if value in (80, 90] ... F if value <= 50
+     * 
+     * @param percentile in [0, 100]
+     * @return a letter in [A, F]
+     */
+    public static char vote(double percentile) {
+        
+    		if (percentile <=50) {
+    			return 'F';
+    		}else if (percentile >50 && percentile <= 60) {
+    			return 'E';
+    		}else if (percentile >60 && percentile <=70) {
+    			return 'D';
+    		}else if(percentile >70 && percentile<=80) {
+    			return 'C';
+    		}else if (percentile >80 && percentile <= 90) {
+    			return 'B';
+    		}
+    	
+    			return 'A';
+ 
+       
+    }
+
+    /**
+     * Leap year checker
+     * 
+     * @param year
+     * @return true if leap year
+     */
+    public static boolean isLeapYear(int year) {
+        if(year%4==0 && year%100!=0) {
+        return true;
+        }
+        return false;
+    }
+
+    /**
+     * Sort the input parameters
+     * 
+     * @param a
+     * @param b
+     * @param c
+     * @return a sorted array
+     */
+    public static int[] sort(int a, int b, int c) {
+        int[] result = new int[3];
+
+     
+        if(a>b) {
+        	if(a>c){
+        		result[2]=a;
+        		
+        		if(b>c) {
+        		result[1]=b;
+        		result[0]=c;
+            }
+        		result[0]=c;
+        		result[1]=b;
+        		
+        }
+        } 	else if(a<b) {
+        	
+        	if(b>c) {
+        	result[2]=b;
+        	
+        	if (a>c) {
+        		result[0]=c;
+        		result[1]=a;
+        	}
+        		result[0]=a;
+        		result[1]=c;
+        	}
+        }else if (a<b&&b<c) {
+        	result[0]=a;
+        result[1]=b;
+        result[2]=c;
+        }
+        return result;
+    
+}
+    
+}
