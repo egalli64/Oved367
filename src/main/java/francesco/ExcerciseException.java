2c13857dca490e1f38a7522af021bbb56acadec9
Manny <egalli64@gmail.com>
2020-08-14 17:56:16 +0200

    Added missing Exception

---
+++ src/main/java/francesco/ExcerciseException.java
@@ -0,0 +1,10 @@
+package francesco;
+
+public class ExcerciseException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public ExcerciseException(String string) {
+        super(string);
+    }
+
+}
